b45e600df4aede00799fe27fe63bf75832d56b90
GitLab CI <ci@gitlab.com>
2026-04-19 11:07:29 +0000

    Update mmcp to version v0.0.38

--- Formula/mmcp.rb
+++ Formula/mmcp.rb
@@ -1,22 +1,22 @@
 class Mmcp < Formula
   desc "MCP Server for OpenAPI — search and invoke any REST API from AI agents"
   homepage "https://github.com/mambu-gmbh/mmcp-brew"
-  version "v0.0.37"
+  version "v0.0.38"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/mambu-gmbh/mmcp-brew/releases/download/v0.0.37/mmcp-v0.0.37-macos-arm64.tar.gz"
-      sha256 "076cf5349c1b12f8947c4ab147e9f0211283e6e73b936ae81ea4cf3ac80d2072"
+      url "https://github.com/mambu-gmbh/mmcp-brew/releases/download/v0.0.38/mmcp-v0.0.38-macos-arm64.tar.gz"
+      sha256 "26ff211eac784a4dc5cc50ca3007043024ce9702d0a32a54c5f400f93575b78e"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/mambu-gmbh/mmcp-brew/releases/download/v0.0.37/mmcp-v0.0.37-linux-amd64.tar.gz"
-      sha256 "5fe84bff70e952c022d0db19ef62a086e66e7902e52ade4978c552cc82068c85"
+      url "https://github.com/mambu-gmbh/mmcp-brew/releases/download/v0.0.38/mmcp-v0.0.38-linux-amd64.tar.gz"
+      sha256 "e4d798628eab6054850a54790f4dadb810ceae4a9cf5b468ff5ecd07c0d5f9d5"
     elsif Hardware::CPU.arm?
-      url "https://github.com/mambu-gmbh/mmcp-brew/releases/download/v0.0.37/mmcp-v0.0.37-linux-arm64.tar.gz"
-      sha256 "d87054b7cb46e96a96ab1b20c733247024d23083f1236b1f2a08a6d262814811"
+      url "https://github.com/mambu-gmbh/mmcp-brew/releases/download/v0.0.38/mmcp-v0.0.38-linux-arm64.tar.gz"
+      sha256 "08651dd1d6792382cb8b89f57b95ba53e50a3d558f2304bc349df948c001601e"
     end
   end
 
